607be5579fdec21346a09cb37d80a354e994ddc1
Sebastian Acosta <sebasblack.2001@gmail.com>
2022-07-18 16:06:01 -0600

    miweb

--- webempresa/settings.py
+++ webempresa/settings.py
@@ -56,7 +56,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-
 ]
 
 ROOT_URLCONF = 'webempresa.urls'
