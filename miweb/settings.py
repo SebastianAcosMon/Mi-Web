55b540e470646b33b3fea3e6eaa93d2a5566d130
Sebastian Acosta <sebasblack.2001@gmail.com>
2022-07-18 13:10:12 -0600

    listos

--- miweb/settings.py
+++ miweb/settings.py
@@ -156,7 +156,6 @@ EMAIL_PORT = '2525'
 import dj_database_url  
 db_from_env = dj_database_url.config(conn_max_age=500)  
 DATABASES['default'].update(db_from_env)
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
